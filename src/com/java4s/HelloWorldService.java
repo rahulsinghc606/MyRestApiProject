92908a11e0e72343e286a58c33107b4881a1e746
Rahul.Singh <rahulsinghc606@gmail.com>
2017-09-13 13:59:27 +0530

    My FirtCommit In Github

---
+++ src/com/java4s/HelloWorldService.java
@@ -0,0 +1,30 @@
+package com.java4s;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+ 
+@Path("/customers")
+public class HelloWorldService {
+ 
+	@GET
+	@Produces("text/html")
+	public Response getLocalCust() { 
+		
+		String output = "<font face='verdana' size='2'> I am from 'getLocalCust' method </font>"; 
+		return Response.status(200).entity(output).build();
+ 
+	}
+	
+	@GET
+	@Path("/nri")
+	@Produces("text/html")
+	public Response getNriCust() {
+ 
+		String output = "<font face='verdana' size='2'> I am from 'getNriCust' method </font>"; 
+		return Response.status(200).entity(output).build();
+ 
+	} 
+	
+}
